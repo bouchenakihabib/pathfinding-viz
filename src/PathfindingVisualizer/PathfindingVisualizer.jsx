64ac7ed2972df4af799d7a16c4ab904a6a72e96c
habibbouchenaki <bouchenaki.habib@yahoo.fr>
2022-04-20 18:21:53 +0200

    navbar added

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -394,16 +394,6 @@ export default class PathfindingVisualizer extends Component {
           <a className="navbar-brand" href="/">
             <b>PathFinding Visualizer</b>
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
         </nav>
 
         <table
